9dfd7ce9d6d8661e3fdde73a8e50a6d1e5789de4
Yarden <jorden.a17@gmail.com>
2025-09-11 14:51:06 +0300

    put & delete req

--- server/routes/ideas.js
+++ server/routes/ideas.js
@@ -56,4 +56,32 @@ router.post('/', (req,res) => {
     res.json({success: true, data:idea});
 });
 
+// Update idea 
+router.put('/:id', (req,res)=> {
+    const idea = ideas.find((idea) => idea.id === +req.params.id)
+
+    if(!idea) {
+        return res.status(400).json({success:false, error:'Resource not found'})
+    }
+
+    idea.text = req.body.text || idea.text;
+    idea.tag = req.body.tag || idea.tag;
+
+    res.json({success: true, data: idea});
+});
+
+// Delete idea
+router.delete('/:id', (req,res)=> {
+    const idea = ideas.find((idea) => idea.id === +req.params.id)
+
+    if(!idea) {
+        return res.status(400).json({success:false, error:'Resource not found'})
+    }
+
+    const index = ideas.indexOf(idea);
+    ideas.splice(index, 1);
+
+    res.json({success: true, data: {}});
+});
+
 module.exports = router;
